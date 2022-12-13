7e5ff32d6db123802e637319227ab8080078c235
Rolly Maulana Awangga <awangga@ulbi.ac.id>
2022-12-13 10:01:26 +0700

    Initial commit

---
+++ app/src/main/java/com/example/asyncapp/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.asyncapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Key for saving the state of the TextView
+    private static final String TEXT_STATE = "currentText";
+
+    // The TextView where we will show results
+    private TextView mTextView = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTextView = findViewById(R.id.textView1);
+
+        // Restore TextView if there is a savedInstanceState
+        if(savedInstanceState!=null){
+            mTextView.setText(savedInstanceState.getString(TEXT_STATE));
+        }
+    }
+
+    public void startTask(View view) {
+        // Put a message in the text view
+        mTextView.setText(R.string.app_name);
+
+        // Start the AsyncTask.
+        new MyAsyncTask(mTextView).execute();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // Save the state of the TextView
+        outState.putString(TEXT_STATE, mTextView.getText().toString());
+    }
+}
